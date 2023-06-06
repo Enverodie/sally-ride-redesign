e142271ae5e254dee40ecfa8db38dd93bdd26a90
enverodie <enverodie@gmail.com>
2023-06-06 15:02:45 -0400

    Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.old.css';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <title></title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Jockey+One&family=Jura:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Helmet>
+      <div className="App">
+
+        {/* <h1>This is a route test</h1>
+        <Routes>
+          <Route path="/" element={<div>root or home</div>} />
+          <Route path="nonHome" element={<b>not home</b>} />
+          <Route path="*" element={<i>not recognized</i>} />
+        </Routes>
+        <h2>Now I'll be inserting a second routes component</h2>
+        <Routes>
+          <Route path="/" element={<div>still at home</div>} />
+          <Route path="nonHome" element={<div>Heyo</div>} />
+        </Routes> */}
+        {/* 
+        
+        App controls the look of every single page, regardless of route.
+        
+      */}
+
+      </div>
+    </>
   );
 }
 
