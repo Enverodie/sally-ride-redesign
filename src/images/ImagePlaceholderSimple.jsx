09823cc2b1eaae50f51cf8ae8c70935e7413020d
enverodie <enverodie@gmail.com>
2023-06-06 22:33:36 -0400

    Added ability to rotate image

--- src/images/ImagePlaceholderSimple.jsx
+++ src/images/ImagePlaceholderSimple.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import './images.scss';
 
-function ImagePlaceholderSimple({src, alt, width, height, children}) {
+function ImagePlaceholderSimple({src, alt, width, height, rotation, children}) {
 
     const [isActive, setActive] = useState(false);
 
@@ -11,7 +11,13 @@ function ImagePlaceholderSimple({src, alt, width, height, children}) {
     }
 
     return ( 
-        <div className="ImagePlaceholder ImagePlaceholderSimple" style={{width: width || "250px", height: height || "250px"}}>
+        <div 
+            className="ImagePlaceholder ImagePlaceholderSimple" 
+            style={{
+                width: width || "250px", 
+                height: height || "250px", 
+                transform: `rotate(${rotation || "0deg"})`,
+            }}>
             <div className={'content' + (isActive? ' active' : '')}>
                 <img className='backgroundImage' src={src} alt={alt} />
                 <div className='info'>
