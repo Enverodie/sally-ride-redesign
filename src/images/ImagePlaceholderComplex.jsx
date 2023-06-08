2fab126509487815b35d83c81d401437b0d4fd35
enverodie <enverodie@gmail.com>
2023-06-08 15:55:09 -0400

    Finished complex image placeholder + arrow + label

--- src/images/ImagePlaceholderComplex.jsx
+++ src/images/ImagePlaceholderComplex.jsx
@@ -1,13 +1,16 @@
-import { useRef } from "react";
-// import Xarrow from "react-xarrows";
+import { useState } from "react";
 import Arrow, { DIRECTION } from 'react-arrows';
-import PaintImg from './watercolor_2.svg';
+import PaintImg from './smear_2.png';
 
 function ImagePlaceholderComplex({src, alt, width, height, rotation, children}) {
 
-    const mainElement = useRef(null);
-    const labelElement = useRef(null);
+    const [isHovered, setHovered] = useState(false);
+    
+    function onHoverOverImage(cursorOn) {
+        setHovered(cursorOn);
+    }
 
+    // fine tuned arrow end directions
     /*
     let toBottom = {
         direction: DIRECTION.BOTTOM,
@@ -15,61 +18,56 @@ function ImagePlaceholderComplex({src, alt, width, height, rotation, children}) 
         translation: [-.2, 2],
     };
     */
-    
+
     let toLeft = {
-        direction: DIRECTION.LEFT,
-        node: () => document.getElementById('mainElement'),
-        translation: [-.5, 0],
+        direction: DIRECTION.BOTTOM_LEFT,
+        node: () => document.getElementById('mainElementPointerBox'),
+        translation: [-.3, .4],
     }
 
+    const arrow = (
+        <Arrow
+            className="ImagePlaceholderComplex arrow"
+            from={{
+                direction: DIRECTION.BOTTOM,
+                node: () => document.getElementById('labelElement'),
+                translation: [.5, 1.3],
+            }}
+            to={toLeft}
+        />
+    )
+
     return ( 
+
         <div 
-            id="mainElement"
-            className="ImagePlaceholder ImagePlaceholderComplex" 
+            className={"ImagePlaceholderComplex" + (isHovered? " hovered" : "")}
+            onMouseEnter={() => onHoverOverImage(true)}
+            onMouseLeave={() => onHoverOverImage(false)}
+            // for some reason we have to include the styles here instead of in the stylesheet
+            // because there will be clipping with the arrow if done in the stylesheet
             style={{
-                width: width || "250px", 
-                height: height || "250px", 
-                left: "600px",
-                transform: `rotate(${rotation || "0deg"})`,
-        }}>
-            <div 
-                ref={mainElement} 
-                className="content"
-            >
-                <img className="backgroundImage" src={src} alt={alt} />
+                width: "fit-content",
+                height: "100%",
+                position: "relative",
+                padding: "1em",
+                boxSizing: "border-box",
+            }}
+        >
+            <div id="mainElement" className="ImagePlaceholder" style={{ width: width || "250px", height: height || "250px", transform: `rotate(${rotation || "0deg"})` }}>
+                <div className="content">
+                    <div id="mainElementPointerBox" class="mainElementPointerBox" style={{transform: `rotate( calc(${'-1 *' + rotation || '0deg'}) )`}}></div>
+                    <img className="backgroundImage" src={src} alt={alt} />
+                </div>
             </div>
-            <div 
-                ref={labelElement}
-                id="labelElement"
-                className="label"
-                style={{transform: `rotate(${-rotation || "0deg"})`
-            }}>
+
+            <div id="labelElement" className="label">
                 <img className="labelbackground" src={PaintImg} alt="" aria-hidden="true" />
                 <div className="labeltext">
                     {children}
                 </div>
             </div>
-            {/* <Xarrow
-                start={labelElement}
-                end={mainElement} 
-                startAnchor={{position: "bottom", offset: {y: 100}}}
-                endAnchor={{position: "bottom"}}
-                color="var(--defaultwhite)"
-                dashness={true}
-                path="grid"
-                curveness="30"
-            /> */}
-            <Arrow
-                className="ImagePlaceholderComplex arrow"
-                from={{
-                    direction: DIRECTION.BOTTOM,
-                    node: () => document.getElementById('labelElement'),
-                    // translation: [.4, 3],
-                    translation: [.5, 1.3],
-                }}
-                to={toLeft}
-                // onChange={}
-            />
+
+            {isHovered && arrow}
         </div>
      );
 }
