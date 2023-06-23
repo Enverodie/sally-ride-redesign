b72f162b31b9bb8bceb913c09dd7041ba51b1905
enverodie <enverodie@gmail.com>
2023-06-23 18:13:03 -0400

    Implemented new custom dropdown

---
+++ src/buttons/GenericDropdown.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import './buttons.scss';
+
+/**
+ * 
+ * @param {String[]} options a list of options to fill the dropdown
+ * @param {Function} onOptionSelect a function executed with the supplied index of the active option
+ */
+function GenericDropdown({options, onOptionSelect}) {
+
+    const [ optionsMenuOpen, setOptionsMenuOpen ] = useState(false);
+    const [ activeOptionIndex, setActiveOptionIndex ] = useState(0);
+
+    function toggleOptionsMenu(e) {
+        if (optionsMenuOpen) e.target.blur();
+        setOptionsMenuOpen(!optionsMenuOpen);
+    }
+
+    function clickOption(i) {
+        if (typeof onOptionSelect === 'function') onOptionSelect(i);
+        setOptionsMenuOpen(false);
+        setActiveOptionIndex(i);
+    }
+
+    return ( 
+        <div className={"GenericDropdown" + (optionsMenuOpen? ' active' : '')}>
+            <button className="selector" onClick={toggleOptionsMenu}>
+                {options[activeOptionIndex]}
+            </button>
+            <div className="options">
+                {options.map((o, i) => {
+                    return ( <button className="option" onClick={() => clickOption(i)}>{o}</button> )
+                })}
+            </div>
+        </div>
+     );
+}
+
+export default GenericDropdown;
